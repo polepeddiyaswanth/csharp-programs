1ab1fd077b1c09fa25d09204bd05fc23cce7e03c
yash <yaswanthpolepeddi675@gmail.com>
2021-08-15 03:49:14 +0530

    C SHARP

--- yashwanth c sharp programs/2d array/Program.cs
+++ yashwanth c sharp programs/2d array/Program.cs
@@ -4,9 +4,15 @@ namespace _2d_array
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-           
+           int[,] array = new int[100,100];
+            int n1,n2;
+            Console.Write("enter the range of first array:");
+            n1=int.Parse(Console.ReadLine());
+            Console.Write("enter the range of second array:");
+            n2=int.Parse(Console.ReadLine());
+            Console.ReadKey(); 
         }
     }
 }
