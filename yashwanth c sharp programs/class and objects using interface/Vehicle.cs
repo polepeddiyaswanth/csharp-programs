a62584c2cde6910b538db6a96dd0f60684880d75
yash <yaswanthpolepeddi675@gmail.com>
2021-08-31 00:17:56 +0530

    CSHARP

---
+++ yashwanth c sharp programs/class and objects using interface/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_and_objects_using_interface
+{
+    interface IVehicle
+    {
+         void Vehicletype();
+    }
+}
